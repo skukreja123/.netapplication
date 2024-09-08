6ac2d0ec9c8b7c387e31b8eeca8700b21da7546b
Sahil Kukreja <k214696@nu.edu.pk>
2024-09-09 02:08:02 +0500

    Initial commit of HelloWorldApp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, .NET World!");
+            Console.WriteLine("Welcome to this simple .NET application.");
+        }
+    }
+}
